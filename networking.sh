ae75f450248479b4017636d5d85f60e55930aec0
renuka123 <sunitapachghare@gmail.com>
2026-06-30 17:38:24 +0530

    notworking

---
+++ networking.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List all VPC networks
+gcloud compute networks list
+
+# Create a VPC network
+gcloud compute networks create my-vpc --subnet-mode=auto
+
+# List subnetworks
+gcloud compute networks subnets list
+
+# Create a firewall rule to allow HTTP traffic
+gcloud compute firewall-rules create allow-http \
+    --network=my-vpc \
+    --allow=tcp:80
+
+# List firewall rules
+gcloud compute firewall-rules list
+
+# Delete a firewall rule
+gcloud compute firewall-rules delete allow-http --quiet
